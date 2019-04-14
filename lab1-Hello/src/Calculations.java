ddc5e5edc08530e2500f29ae9a4783e15714e32a
Chongfeng <chongfeng.zero@gmail.com>
2019-04-14 17:24:34 +0800

    lab1-Hello/

---
+++ lab1-Hello/src/Calculations.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Chongfeng.Ling17
+ */
+public class Calculations {
+    public static void main(String[] args) {
+        
+        System.out.println("5 * 7 + 4 - 2 / 2 = 38");
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+        int num4 = 25;
+        int num5 = 30;
+        System.out.println(num1);
+        int calculation = num1 * num2 + num3 - num4 / num5;
+        System.out.println(calculation);
+
+    }
+}
---
+++ lab1-Hello/src/lab1/hello/Lab1Hello.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.hello;
+
+/**
+ *
+ * @author Chongfeng.Ling17
+ */
+public class Lab1Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Chongfeng Ling");
+        System.out.println("Hello Joe");
+        System.out.println("My name is Chongfeng Ling, and I'm a student, my major is Applied Mathematics.");
+        System.out.println("My name is Chongfeng Ling, my school is XJTLU, and my hometown is Taizhou.");
+        System.out.println("Name: Chongfeng Ling, Age: 32, Student number: 1716474");
+        
+    }
+    
+}
